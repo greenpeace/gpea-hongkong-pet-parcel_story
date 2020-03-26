01e6cb4b82fbf7a93b64988418143a0735550cd4
KFC <hankfchan@gmail.com>
2020-03-26 20:12:35 +0800

    Updates

--- main.f7057b06.js
+++ main.f7057b06.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"KIzB":[function(require,module,exports) {
 "use strict";require("flexboxgrid/css/flexboxgrid.min.css"),console.log("main.js");
 },{"flexboxgrid/css/flexboxgrid.min.css":"JHU5"}]},{},["KIzB"], null)
-//# sourceMappingURL=/main.f7057b06.js.map
+//# sourceMappingURL=main.f7057b06.js.map
