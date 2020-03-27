a25edd184ef25dd5dc86cfe11df22bbd0c2cdee8
KFC <hankfchan@gmail.com>
2020-03-27 14:35:16 +0800

    structing the css components

--- postcss.config.js
+++ postcss.config.js
@@ -14,6 +14,7 @@ const purgecss = require('@fullhuman/postcss-purgecss')({
 
 module.exports = {
   plugins: [
+    require('postcss-import'),
     require('tailwindcss')('./tailwindcss.config.js'),
     require('autoprefixer'),
     ...(process.env.NODE_ENV === 'production' ? [purgecss] : []),
