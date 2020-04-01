2ab17b4d9bdee11e89f2ed0fdce372153427e6d5
KFC <hankfchan@gmail.com>
2020-04-01 14:10:18 +0800

    init the preference page

--- tailwindcss.config.js
+++ tailwindcss.config.js
@@ -40,6 +40,12 @@ module.exports = {
         gpgreen: '#66cc00',
         gporange: '#ff9500',
         gpdarkblue: '#292f47',
+        arctic: '#62cbd7',
+        health: '#f96d8c',
+        plastics: '#afaa91',
+        climate: '#ffbe00',
+        forests: '#00b474',
+        oceans: '#008fe2',
       },
       borderRadius: {
         xl: '1rem',
