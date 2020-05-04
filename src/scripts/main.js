7ee7c08a5d6b4e8b5d485331ca74e9d12123132c
KFC <hankfchan@gmail.com>
2020-05-04 13:29:49 +0800

    adding vendors

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,21 @@
 'use strict';
+import 'remove-focus-outline';
 import SmoothScroll from 'smooth-scroll';
+import Swiper from 'swiper';
 //
+import polyfill from 'polyfill';
+polyfill();
 document.addEventListener('DOMContentLoaded', function (event) {
-  var scroll = new SmoothScroll('a[href*="#"]');
+  new LazyLoad({
+    elements_selector: '.lazy',
+    threshold: 400,
+  });
+
+  const Scroll = new SmoothScroll('a[href*="#"]');
+
+  AOS.init({
+    easing: 'ease-in-out-cubic',
+    once: true,
+    disable: 'phone',
+  });
 });
